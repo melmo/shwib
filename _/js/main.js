6f338b71397ba371ab28528adef45d00d406a7aa
Melanie Thewlis <melaniethewlis@littlewebgiants.com>
2015-02-02 14:51:00 +0100

    Drop down menu click closes other open menus

--- _/js/main.js
+++ _/js/main.js
@@ -1,6 +1,11 @@
 jQuery(document).ready(function($) {
 	$('#menu-primary-items .dropdown > a').on("click", function(e) {
 		e.preventDefault();
-		$(this).parent('li').toggleClass("open");
+		if($(this).parent('li').hasClass("open");) {
+			$(this).parent('li').removeClass("open");
+		} else {
+			$('#menu-primary-items .dropdown').removeClass('open');
+			$(this).parent('li').addClass("open");
+		}
 	});
 });
